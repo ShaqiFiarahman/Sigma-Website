5d035cf549f836687e590e274c4be25d963d600b
zibro Z809 <ziboz809@gmail.com>
2026-05-14 09:48:07 +0700

    remove android-assets from git tracking (keep locally)

--- resources/android-assets/app/src/main/java/com/mahasiswa/sigma/ui/navigation/Compositions.kt
+++
@@ -1,7 +0,0 @@
-package com.mahasiswa.sigma.ui.navigation
-
-import androidx.compose.runtime.compositionLocalOf
-
-val LocalBackStack = compositionLocalOf<MutableList<Route>> {
-    error("No BackStack provided")
-}
