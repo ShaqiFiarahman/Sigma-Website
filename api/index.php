4f8384810b7a568fc4037aaae70cf5a56c0e07d2
ShaqiFiarahman <fadelshaqif@gmail.com>
2026-06-05 15:34:11 +0700

    fix: remove outputDirectory to include all project files

--- api/index.php
+++ api/index.php
@@ -4,5 +4,7 @@ ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
+echo "PHP Serverless is running! Attempting to load Laravel...\n";
+
 // Forward request to Laravel's public/index.php
 require __DIR__ . '/../public/index.php';
